f9fb97540fcbd425e4b1e913a5967670fa5915a4
Jeferson Nochelli <jefersonnochelli@hotmail.com>
2024-04-17 15:21:22 -0300

    a
    a

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ if ('IntersectionObserver' in window) {
     console.log('Seu navegador não suporta IntersectionObserver.');
 }
     // script hidden and show
+
+
+
+    //menu mobile
+function animar(){
+    const btn = document.getElementById('btn-menu')  
+    btn.classList.toggle('ativar')
+
+}
+
+const menuDiv = document.getElementById('menu-mobile')
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativo')
+
+}
+
+
+
